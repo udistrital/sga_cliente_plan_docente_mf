316c3f5292d32630dc9a5a0ae155dea8bf336196
NeFaWaltPint <nefawaltpint@gmail.com>
2024-04-25 09:33:07 -0500

    udistrital/sga_documentacion#176 actualiza url ambientes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,11 @@ export const environment = {
     apiUrl: "http://localhost:4216/",
     PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
     PLAN_TRABAJO_DOCENTE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_trabajo_docente_crud/v1/',
-    SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE: 'http://localhost:8552/v1/',//'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_mid/v1/',
+    SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE: 'http://localhost:8552/v1/',
     ESPACIOS_ACADEMICOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/espacios_academicos_crud/v1/',
     TERCEROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
     OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
-    SGA_ESPACIOS_ACADEMICOS_MID_SERVICE: 'https://localhost:8080/v1/',
+    SGA_ESPACIOS_ACADEMICOS_MID_SERVICE: 'http://pruebasapi3.intranetoas.udistrital.edu.co:8553/',
     GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
     DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
     PROYECTO_ACADEMICO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/proyecto_academico_crud/v1/'
