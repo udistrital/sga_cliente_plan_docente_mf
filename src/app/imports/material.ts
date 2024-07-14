6ea32de58363778e551b436fa2643a2889a61c20
bfmunozm <swsbmm@gmail.com>
2024-07-14 12:20:49 -0500

    feat: agregar MatDividerModule udistrital/sga_documentacion#344

--- src/app/imports/material.ts
+++ src/app/imports/material.ts
@@ -1,43 +1,45 @@
-import { MatCardModule } from '@angular/material/card';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatCardModule } from "@angular/material/card";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 export const MATERIAL_MODULES = [
-    MatCardModule,
-    MatSnackBarModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatTooltipModule,
-    MatCheckboxModule,
-    MatDialogModule,
-    MatAutocompleteModule,
-    DragDropModule,
-    MatDatepickerModule,
-    MatTabsModule,
-    MatSlideToggleModule,
-    MatToolbarModule,
-    MatDividerModule
+  MatCardModule,
+  MatSnackBarModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatAutocompleteModule,
+  DragDropModule,
+  MatDatepickerModule,
+  MatTabsModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatDividerModule,
+  MatGridListModule,
 ];
