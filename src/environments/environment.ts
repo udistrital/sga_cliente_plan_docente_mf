d0755efc0bd64dc3dbdd5af9bad70d191da47e07
diagutierrezro <diagutierrezro@unal.edu.co>
2026-02-04 17:25:30 -0500

    fix: Ajuste de variables de entorno horarios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -20,6 +20,6 @@ export const environment = {
     "https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/",
   PROYECTO_ACADEMICO_SERVICE:
     "https://autenticacion.portaloas.udistrital.edu.co/apioas/proyecto_academico_crud/v1/",
-  HORARIO_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/horario_mid/v1/",
+  HORARIO_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/horarios_mid/v1/",
   HORARIO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/horarios_crud/v1/",
 };
