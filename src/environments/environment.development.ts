b6425156553719cdbe9229b61ad78031b28ec63e
Nico-Guz <nicolas.riniti@gmail.com>
2026-02-23 17:20:48 -0500

    fix: Ajuste endpoints y variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: "https://pruebassgaplandocente.portaloas.udistrital.edu.co/",
+    NUXEO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
     PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
     PLAN_TRABAJO_DOCENTE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_trabajo_docente_crud/v1/',
     SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_trabajo_docente_mid/v1/',
@@ -8,9 +9,10 @@ export const environment = {
     TERCEROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
     OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
     SGA_ESPACIOS_ACADEMICOS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_espacios_academicos_mid/v1/',
-    GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
+    FIRMA_ELECTRONICA_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/',
+    GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
     DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
     PROYECTO_ACADEMICO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/proyecto_academico_crud/v1/',
     HORARIO_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/horarios_mid/v1/",
-      HORARIO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/horarios_crud/v1/",
+    HORARIO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/horarios_crud/v1/",
 }
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: "https://sgaplandocente.portaloas.udistrital.edu.co/",
+    NUXEO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
     PARAMETROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/parametros/v1/',
     PLAN_TRABAJO_DOCENTE_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_trabajo_docente_crud/v1/',
     SGA_PLAN_TRABAJO_DOCENTE_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/plan_trabajo_docente_mid/v1/',
@@ -8,7 +9,8 @@ export const environment = {
     TERCEROS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/terceros_crud/v1/',
     OIKOS_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v2/',
     SGA_ESPACIOS_ACADEMICOS_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/sga_espacios_academicos_mid/v1/',
-    GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1',
+    FIRMA_ELECTRONICA_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/',
+    GESTOR_DOCUMENTAL_MID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1/',
     DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
     PROYECTO_ACADEMICO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/proyecto_academico_crud/v1/'
 }
